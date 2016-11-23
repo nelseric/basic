f65b316acbbaa0ded17a34b336a65bc18d4de71a
Eric Nelson <nelseric.alt@gmail.com>
2016-11-23 16:29:54 -0500

    Add some routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Match, Miss, Link } from 'react-router'
+
+import Inspector from 'react-inspector';
+
+import Hello from './Hello';
+
 import logo from './logo.svg';
 import './App.css';
+
+
+const Home = ({location}) => {
+  return (
+    <h3>Welcome to my game.</h3>
+  );
+}
+
+const NoMatch = ({location}) => {
+  return (
+    <div>
+      <h3>There's nothing here.</h3>
+      <Inspector data={location} />
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/hello">Hello</Link>
+              </li>
+              <li>
+                <Link to="/faq">FAQ</Link>
+              </li>
+            </ul>
+          </div>
+          <p className="App-intro">
+            <Match pattern='/' exactly component={Home}/>
+            <Match pattern='/hello' component={Hello}/>
+            <Miss component={NoMatch} />
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
